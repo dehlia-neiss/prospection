d5bcc2b4d5fcb0869bce2707180b3abe67b60e7f
dehlia-neiss <dehlia.neiss@laplateforme.io>
2025-12-09 14:08:28 +0100

    clean structure

--- tableau-entreprises/frontend/src/MapProspect.jsx
+++ tableau-entreprises/frontend/src/MapProspect.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import './MapProspects.css';
+import './MapProspect.css';
 
 // Fix des icônes Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
